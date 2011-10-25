a453e91794e88918bff6bb18205706fc0354eac9
unknown <Diogo@.(none)>
2011-10-25 21:30:41 +0100

    teste login corrigido

--- features/support/env.rb
+++ features/support/env.rb
@@ -48,3 +48,13 @@ end
 #     DatabaseCleaner.strategy = :transaction
 #   end
 #
+
+
+# LOAD FIXTURES
+
+Before do
+  ActiveRecord::Fixtures.reset_cache
+  fixtures_folder = File.join(Rails.root, 'test', 'fixtures')
+  fixtures = Dir[File.join(fixtures_folder, '*.yml')].map {|f| File.basename(f, '.yml') }
+  ActiveRecord::Fixtures.create_fixtures(fixtures_folder, fixtures)
+end
