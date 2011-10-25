c105a53993021bb78f563b409a2b52c86a253524
Diogo Castro <b.boy.souljah@gmail.com>
2011-10-25 10:40:21 +0100

    project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mads2011::Application.config.secret_token = '46112abfdaee98a730ad1bbb46c16b8f0ed9267578fcdea15a88ce1cdb73b683277d633db6514f6637e25d5f4ff1e80f2cd6600c80fa42201bb8faecb576862d'
