142bf7c55cf88934a3e07edc94fe85e473818479
Diogo Castro <b.boy.souljah@gmail.com>
2011-11-07 21:18:23 +0000

    feature index na pagina do projecto

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,17 +11,10 @@ Project.delete_all
 Role.delete_all
 
 
-<<<<<<< HEAD
-@admin = User.create! username: "admin", password: "admin", name: "Sereno", email: "admin@gmail.com", contact: "91 101 01 01"
-@proj = Project.create! name: "Project XPTO", repo: "http://github.com/dcastro/mads2011.git"
-=======
 @admin = User.create! username: "admin", password: "admin", name: "Sereno", email: "b.boy.souljah@gmail.com", contact: "91 101 01 01"
 @yuno = User.create! username: "yuno", password: "1234", name: "Yuno", email: "xyunox@gmail.com", contact: "91 11 11 111"
 
-@proj = Project.create! name: "Project XPTO", repo: "github.com/dcastro/mads2011.git"
->>>>>>> c8a2ab99347c282c984ea7e8b5aa0cd39e799e06
-
-
+@proj = Project.create! name: "Project XPTO", repo: "http://github.com/dcastro/mads2011.git"
 
 @role = Role.create! user: @admin, project: @proj, name: "Manager"
 @role = Role.create! user: @yuno, project: @proj, name: "Developer"
