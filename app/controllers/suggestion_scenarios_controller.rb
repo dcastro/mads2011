d08b6391ff2d660229d042c4453b785c58c6219e
Diogo Castro <b.boy.souljah@gmail.com>
2011-11-30 12:06:51 +0000

    corrigidos erros da criacao de sugestoes

--- app/controllers/suggestion_scenarios_controller.rb
+++ app/controllers/suggestion_scenarios_controller.rb
@@ -9,17 +9,23 @@ class SuggestionScenariosController < ApplicationController
       @sc.suggestion_steps.build keyword: keyword, name: name
     end
     
-    params[:row].each do |index, rows|
-      rows.each do |row_num, cells|
-        @sc.suggestion_steps[index.to_i].suggestion_rows.build order: row_num
-        
-        for i in 0..(cells.length-1)
-          @sc.suggestion_steps[index.to_i].suggestion_rows.last.suggestion_cells.build order: i, name: cells[i]
+    if params[:row]
+      params[:row].each do |index, rows|
+        rows.each do |row_num, cells|
+          @sc.suggestion_steps[index.to_i].suggestion_rows.build order: row_num
+          
+          for i in 0..(cells.length-1)
+            @sc.suggestion_steps[index.to_i].suggestion_rows.last.suggestion_cells.build order: i, name: cells[i]
+          end
         end
       end
-    end
+    end    
     
-    @sc.save!
+    if @sc.save
+      redirect_to features_show_url(id: @sc.feature_id )
+    else
+      redirect_to features_show_url(id: @sc.feature_id ), notice: "Invalid Suggestion"
+    end
     
   end
 
