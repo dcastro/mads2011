53e6ecedd1b489a034fd1de267aaaf43a3dbc1af
Diogo Castro <dcastro@ubuntu.(none)>
2011-12-20 14:39:17 +0000

    Test: Comment on a scenario / Comment on a step

--- features/comments.feature
+++ features/comments.feature
@@ -4,11 +4,30 @@ Feature: Leave a comment
 	I want to be able to comment on features, scenarios and steps
 	
 	@javascript
-	Scenario: Comment on a scenario
+	Scenario: Comment on a feature
 		Given I am logged in as "admin" with password "admin"
-		Given I am on the feature "Fixture Feature" page
+		And I am on the feature "Fixture Feature" page
 		When I leave a comment saying "I think this feature should be removed altogether."
 		Then the feature should have 1 comment
 		When I delete the comment
 		Then the feature should have 0 comments
+	
+	@javascript	
+	Scenario: Comment on a scenario
+		Given I am logged in as "admin" with password "admin"
+		And I am on the feature "Fixture Feature" page
+		When I leave a comment on scenario "Dummy Scenario" saying "This scenario is very appropriate."
+		Then the scenario should have 1 comment
+		When I delete the comment
+		Then the scenario should have 0 comments
+		
+		
+	@javascript
+	Scenario: Comment on a scenario step
+		Given I am logged in as "admin" with password "admin"
+		And I am on the feature "Fixture Feature" page
+		When I leave a comment on scenario "Dummy Scenario"'s first step saying "I think i know why this step is failing."
+		Then the step should have 1 comment
+		When I delete the comment
+		Then the step should have 0 comments
 		
--- features/step_definitions/comment_steps.rb
+++ features/step_definitions/comment_steps.rb
@@ -7,9 +7,48 @@ When /^I leave a comment saying "([^"]*)"$/ do |content|
 end
 
 
+When /^I leave a comment on scenario "([^"]*)" saying "([^"]*)"$/ do |scenario_name, content|
+  
+  #display scenario comments
+  @scenario = @feature.scenarios.find_by_name(scenario_name)
+  sleep 1
+  page.driver.browser.execute_script %Q{ $('#Scenario#{ @scenario.id }').trigger("mouseenter").click(); }
+  sleep 1
+  
+  #leave comment
+  step %{I leave a comment saying "#{ content }"}
+  
+end
+
+When /^I leave a comment on scenario "([^"]*)"'s first step saying "([^"]*)"$/ do |scenario_name, content|
+  @step = @feature.scenarios.find_by_name(scenario_name).steps.first
+  
+  #open scenario tab
+  sleep 1
+  page.driver.browser.execute_script %Q{ $('h5:contains("#{scenario_name}")').trigger("mouseenter").click(); }
+  
+  #display step comments
+  sleep 1
+  step %{I follow "#{ @step.name }"}
+  sleep 1
+  
+  #leave comment
+  step %{I leave a comment saying "#{ content }"}  
+  
+end
+
+
+
 Then /^the feature should have (\d+) comments?$/ do |n_comments|
   assert_equal n_comments.to_i, @feature.comments.count
-  
+end
+
+Then /^the scenario should have (\d+) comments?$/ do |n_comments|
+  assert_equal n_comments.to_i, @scenario.comments.count
+end
+
+Then /^the step should have (\d+) comments?$/ do |n_comments|
+  assert_equal n_comments.to_i, @step.comments.count
 end
 
 When /^I delete the comment$/ do
