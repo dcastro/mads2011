9cd39edb8acfc58901033a8ec3ed56522c0d81ed
Diogo Castro <dcastro@ubuntu.(none)>
2012-01-02 15:56:35 +0000

    prevent from adding duplicate roles

--- app/models/role.rb
+++ app/models/role.rb
@@ -5,6 +5,8 @@ class Role < ActiveRecord::Base
   ROLE_TYPES = ["Manager", "Developer", "Client"]
   validates :name, :inclusion => ROLE_TYPES
   
+  validates :user_id, :uniqueness => {:scope => :project_id}
+  
   def self.role_types
     ROLE_TYPES
   end
